deb0313232b0cb35f449d6d7830294097b8176cd
user.email : "prakashdhamdhere7350@gmail.com <user.email : prakashdhamdhere7350@gmail.com>
2026-02-21 12:46:52 +0530

    api updated

--- Frontend/src/utils/Axois.jsx
+++ Frontend/src/utils/Axois.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://mern-stack-note-app-so6x.onrender.com",
     withCredentials: true
 })
 
